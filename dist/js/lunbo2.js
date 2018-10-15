6bfa8098864574b082dc337dd01cf5dd2b9b2749
xiaokangkang123 <1903259010@qq.com>
2018-10-15 19:13:00 +0800

    2018/10/15 19:12 提交

---
+++ dist/js/lunbo2.js
@@ -0,0 +1,76 @@
+function Banner(){}
+$.extend(Banner.prototype,{
+    //初始化
+    init(opt){
+        // 选择元素
+        this.btn_list=$(opt.btn_list);
+        // console.log(this.btn_list)
+        this.li_list=$(opt.li_list);
+        this.ul=$(".hot-down-ul1");
+        // console.log(this.ul);
+        this.wrap=$(".hot-down");
+        // console.log(this.wrap);
+        // li的宽度
+        this.li_width=this.li_list.width();
+        // console.log(this.li_width);
+        //li_list的长度
+        this.liIndex=this.li_list.length;
+        // console.log(this.liIndex);
+        // 当前下标
+        this.nowIndex=0;
+        //调用核心功能
+        this.bindEvent();
+    },
+    //绑定事件
+    bindEvent(){
+        this.btn_list.mouseover($.proxy(this.toIndex,this));
+        this.wrap.mouseenter($.proxy(this.stop,this));
+        this.wrap.mouseleave($.proxy(this.autoPlay,this))
+    },
+    //下一张图片
+    next(){
+        //判断是否到最后一张
+        if(this.nowIndex==this.liIndex-1){
+            this.nowIndex=1;
+            this.ul.css({
+                left:0
+            })
+        }else{
+            this.nowIndex++;
+        }
+        //调用动画
+        this.animate();
+    },
+    //到哪张
+    toIndex(event){
+        var target=event.target||event.srcElement;
+        this.nowIndex=$(target).index();
+        this.animate();
+    },
+    //动画
+    animate(){
+        this.ul.stop().animate({
+            left:-this.li_width*this.nowIndex
+        });
+        var index=this.nowIndex==this.liIndex-1?0:this.nowIndex;
+        this.btn_list.eq(index).addClass("active")
+        .siblings("button").removeClass("active")
+    },
+    //自动轮播
+    autoPlay(){
+        var timer=setInterval(function(){
+            this.next()
+        }.bind(this),3000)
+    },
+    //停止轮播
+    stop(){
+        clearInterval(this.timer);
+    }
+})
+//实例化对象
+var banner=new Banner();
+banner.init({
+    btn_list:".hot-top-right1 .hot-top-circle1",
+    li_list:".hot-down-li1"
+})
+banner.autoPlay();
---
+++ gulpfile.js
@@ -0,0 +1,84 @@
+const gulp=require("gulp");
+// 转存css
+gulp.task("css",()=>{
+    return gulp.src(["css/*.css"])
+    .pipe(gulp.dest("dist/css"))
+});
+// 压缩css
+// var cleanCss=require("gulp-clean-css")
+// gulp.task("css",()=>{
+//     return gulp.src(["styles/*.css"])
+//     .pipe(cleanCss())
+//     .pipe(gulp.dest("dist/css"))
+// });
+// 实时刷新
+gulp.task("html",()=>{
+    return gulp.src("index.html").pipe(gulp.dest("dist/"))
+    .pipe(connect.reload())
+});
+gulp.task("watch",()=>{
+    gulp.watch("index.html","html")
+    // gulp.watch("sass/*.scss",["html","sass"])
+});
+const connect=require("gulp-connect");
+gulp.task('connect',function(){
+    connect.server({
+        port:8888,
+        root:"dist/",
+        // 中间件;反向代理
+        middleware:function(connect , opt){
+            var Proxy = require('gulp-connect-proxy');
+            opt.route = '/proxy';
+            var proxy = new Proxy(opt);
+            return [proxy];
+        },
+        livereload:true
+    })
+});
+gulp.task("default",["watch","connect"])
+
+//js的转存指令script
+gulp.task("script",()=>{
+    return gulp.src("js/*.js")
+    .pipe(gulp.dest("dist/js"))
+})
+//转存images
+gulp.task("images",()=>{
+    return gulp.src(["images/*jpg","images/*.png","images/*.gif","images/*.jpeg"])
+    .pipe(gulp.dest("dist/images"))
+})
+// gulp合并和压缩
+// var concat = require('gulp-concat');
+// var uglify = require("gulp-uglify");
+// gulp.task("script",()=>{
+//     gulp.src("script/libs/*.js","script/module/*.js","script/app/*.js",
+//     "!script/libs/jqury.js")
+//     //合并为一个js文件
+//     .pipe(gulp.concat("main.js"))
+//     //压缩
+//     .pipe(gulp.uglify())
+//     .pipe(gulp.dest("dist/script"))
+// })
+// "!表示这个文件不压缩
+
+// gulp 服务器插件;
+// const connect = require("gulp-connect");
+// gulp 合并插件;
+// var concat = require('gulp-concat');
+// gulp 压缩插件;
+// var uglify = require("gulp-uglify");
+// babel 插件;
+// var babel = require("gulp-babel");
+
+// 编译：es6=>es5
+// babel插件
+// const gulp=require("gulp");
+// var babel=require("gulp-babel");
+
+// gulp.task("es6",()=>{
+//     return gulp.src("script/es2015/es6.js")
+//     .pipe(babel({
+//         presets:['@babel/env']
+//     }))
+//     .pipe(gulp.dest("dist/script"));
+// })
